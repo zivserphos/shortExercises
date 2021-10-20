b99dab6f770f06e6126fad2511310af453c8cfde
Ziv Serphos <zivfromisrael@gmail.com>
2021-10-20 15:16:25 +0300

    09-Node+WebServer structure

---
+++ 09-Node+WebServer/back/db.js
@@ -0,0 +1,12 @@
+const validStudent = {
+  nameNotEqual: ["Ang", "Momo", "Appa", "Nina", "Superman", "Robinhood"],
+  minAge: 18,
+  maxAge: 35,
+  ability: [
+    "time management",
+    "Self-motivation",
+    "quick learner",
+    "Critical thinking",
+    "Good communication",
+  ],
+};
