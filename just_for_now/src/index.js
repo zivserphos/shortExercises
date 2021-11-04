f7e2609696c53ba3f363308bc4b6ffb688e8bbbd
Ziv Serphos <zivfromisrael@gmail.com>
2021-11-04 10:32:00 +0200

    added directory just_for_now

---
+++ just_for_now/src/index.js
@@ -0,0 +1,16 @@
+import styles from "./styles.scss"
+
+const button = document.querySelector('.button');
+const submit = document.querySelector('.submit');
+
+function toggleClass() {
+	this.classList.toggle('active');
+}
+
+function addClass() {
+	this.classList.add('finished');
+}
+
+button.addEventListener('click', toggleClass);
+button.addEventListener('transitionend', toggleClass);
+button.addEventListener('transitionend', addClass);
